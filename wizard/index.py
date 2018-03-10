ea640d484bada9d0bb05c7f0ee560b9efff88d12
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-03-10 18:30:35 +0100

    Added the list of templates and saves to the wizard.

--- wizard/index.py
+++ wizard/index.py
@@ -1,8 +1,45 @@
 from flask import Flask
-from flask import render_template
+from flask import send_from_directory, jsonify
+from .. import get_template
+
+import glob
+import os.path
+import yaml
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('templates', 'index.html')
+
+
+@app.route('/templates')
+def templates():
+    """List the name and descriptors of every available template."""
+    def get_name(path):
+        parts = os.path\
+            .relpath(path, '../templates')\
+            .split(os.sep)
+        parts[-1] = parts[-1].replace('.yml', '')
+
+        return '.'.join(parts)
+
+    paths = glob.glob('../templates/**/*.yml')
+    names = map(get_name, paths)
+
+    return jsonify({name: get_template(name)[0] for name in names})
+
+
+@app.route('/saves')
+def saves():
+    """List the name and content of every saved diffusion."""
+    def get_name(path):
+        return os.path.split(path)[1].replace('.yml', '')
+
+    def get_content(path):
+        with open(path) as file:
+            return yaml.load(file)
+
+    paths = glob.glob('storage/*.yml')
+    return jsonify({get_name(path): get_content(path) for path in paths})
