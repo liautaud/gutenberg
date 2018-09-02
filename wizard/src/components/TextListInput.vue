e305c455e7f8ac70c4f82b251c4cf99f52ae54cd
Romain Liautaud <romain.liautaud@ens-lyon.fr>
2018-09-02 19:33:43 +0200

    Implemented TextListInput (WIP).

---
+++ wizard/src/components/TextListInput.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <b-tabs type="is-boxed">
+        <b-tab-item v-for="(content, i) in value" :label="i">
+        	<markdown-input v-model="value[i]"></markdown-input>
+        </b-tab-item>
+        <b-tab-item icon="plus"></b-tab-item>
+    </b-tabs>
+
+<!-- 	<b-select placeholder="Cliquez pour choisir une option" expanded v-model="value">
+		<option
+			v-for="(name, value) in definition.choices"
+			:value="value">
+			{{ name }}
+		</option>
+	</b-select>
+ --></template>
+
+<script>
+	import MarkdownInput from './MarkdownInput'
+
+	export default {
+		components: { MarkdownInput },
+		props: {
+			value: Array,
+		},
+
+		methods: {
+			add() {
+				// this.$emit('input', this.editor.value())
+			},
+
+			remove() {
+				// this.$emit('input', this.editor.value())
+			}
+		}
+	}
+</script>
